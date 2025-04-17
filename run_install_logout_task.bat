bc8805d224224335d14f44beccc5b3c3bb5e95b9
Michael C. Lehn <michel.lehn@uni-ulm.de>
2025-04-17 17:15:43 +0200

    Run as admin

--- run_install_logout_task.bat
+++ run_install_logout_task.bat
@@ -1,2 +1,7 @@
-powershell -ExecutionPolicy Bypass -File "C:\install_logout_task.ps1"
+@echo off
+set SCRIPT=install_logout_task.ps1
+
+:: Run PowerShell script with admin rights
+echo Running as admin...
+runas /user:Administrator "powershell -ExecutionPolicy Bypass -File \"%SCRIPT%\""
 
